139573aa6ca706352d858d301d8932fefc7ebb2d
osimuka <osimukaokpan@gmail.com>
2023-10-23 13:53:05 +0100

    payment on the go

--- src/pages/CourseInformation.jsx
+++ src/pages/CourseInformation.jsx
@@ -8,15 +8,15 @@ const CourseInformation = () => {
     const pId = params.id - 1;
     return (
         <div className='w-full p-4 text-white'>
-            <div className='my-4'>
+            <div className='my-4 text-[1rem] md:text-xl'>
                 <h1 className='text-2xl font-bold py-8 uppercase'>{data[pId].title}</h1>
                 <img className='w-full  object-cover' src={data[pId].imagelink} alt={data[pId].title} />
-                <p className='py-2 font-bold text-xl text-green-500'>{data[pId].category}</p>
+                <p className='py-2 font-bold text-green-500'>{data[pId].category}</p>
                 <p className='py-8 text-xl'>
                     {data[pId].information}
                 </p>
-                <h2 className='py-2 text-xl'>Things you will learn</h2>
-                <p className='py-8 text-xl'>
+                <h2 className='py-2'>Things you will learn</h2>
+                <p className='py-8'>
                     <ul>
                         {data[pId].list.map(items => {
                             return <li className='mb-1'>{items}</li>
@@ -24,7 +24,7 @@ const CourseInformation = () => {
                     </ul>
                 </p>
                 <h2 className='text-center text-2xl font-bold mb-2'>Price</h2>
-                <div className='bg-green-500 flex justify-around py-4 rounded-xl text-3xl font-semibold'>
+                <div className='bg-green-500 flex justify-around py-4 rounded-xl text-xl md:text-2xl font-semibold'>
                     <p className='line-through'>{data[pId].price1}</p>
                     <p>{data[pId].price2}</p>
                 </div>
--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -1,40 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PaystackPop from '@paystack/inline-js'
 
 const PaymentPage = () => {
+    const [email, setEmail] = useState("");
+    const [amount, setAmount] = useState("")
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("")
+    const payWithPayStack = (e) => {
+        e.preventDefault();
+        const paystack = new PaystackPop();
+        paystack.transactions()
+    }
 
     return (
-        <div className='h-screen'>
-            <div className='text-white'>
-                <div className="item">
-                    <div className="overlay-effect"></div>
-                    <img
-                        className="item-image"
-                        src="https://images.unsplash.com/photo-1526947425960-945c6e72858f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-                        alt="product"
-                    />
-                    <div className="item-details">
-                        <p className="item-details__title">Cotton</p>
-                        <p className="item-details__amount">NGN 1000</p>
-                    </div>
-                </div>
-                <div className="checkout">
-                    <div className="checkout-form">
-                        <div className="checkout-field">
-                            <label>Name</label>
-                            <input />
-                        </div>
-                        <div className="checkout-field">
-                            <label>Email</label>
-                            <input />
-                        </div>
-                        <div className="checkout-field">
-                            <label>Phone</label>
-                            <input />
-                        </div>
-                    </div>
-                </div>
+        <div className='h-auto text-black'>
+            <div className='bg-green-400 flex justify-center items-center text-2xl text-black font-bold py-4 md:py-8'>
+                <h1>Make Payments</h1>
             </div>
+            <form action="" className='shadow-xl p-4 flex flex-col gap-4 md:max-w-md lg:max-w-lg xl:max-w-lg max-w-xs bg-gray-500 mx-auto rounded-b-xl mb-8'>
+                <div className='flex flex-col gap-2' >
+                    <label className='font-semibold' htmlFor="email">Email address</label>
+                    <input className='p-2 rounded-md text-black' value={email} onChange={(e) => { setEmail(e.target.value) }} type="email" id='email-address' />
+                </div>
+                <div className='flex flex-col gap-2'>
+                    <label className='font-semibold' htmlFor="amount">Amount</label>
+                    <input className='p-2 rounded-md text-black' value={amount} onChange={(e) => setAmount(e.target.value)} type="tel" id='amount' />
+                </div>
+                <div className='flex flex-col gap-2'>
+                    <label className='font-semibold' htmlFor="first-name">First name</label>
+                    <input className='p-2 rounded-md text-black' value={firstname} onChange={(e) => setFirstname(e.target.value)} type="text" id='first-name' />
+                </div>
+                <div className='flex flex-col gap-2'>
+                    <label className='font-semibold' htmlFor="last-name">Last name</label>
+                    <input className='p-2 rounded-md text-black' value={lastname} onChange={(e) => setLastname(e.target.value)} type="text" id='last-name' />
+                </div>
+                <div>
+                    <button type='submit' className='bg-green-500 p-3 w-full rounded-md text-black font-semibold' onClick={payWithPayStack}>Pay</button>
+                </div>
+
+            </form>
         </div>
     )
 }

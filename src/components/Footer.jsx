9a1d1941e4e034e0d46c1d7bcc1b096ca4b88fd0
osimuka <osimukaokpan@gmail.com>
2023-10-23 11:20:01 +0100

    sticky

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='bg-black w-full text-green-500 flex justify-center items-center md:py-8 py-6 fixed bottom-0'>
+        <div className='bg-black w-full text-green-500 flex justify-center items-center md:py-8 py-6 sticky bottom-0'>
             <div>
                 <p className='font-bold'>Copyright &copy; Teamjojocode | 2023.</p>
             </div>

c76f5b03c3e966deaac10a17578fe06f86493471
osimuka <osimukaokpan@gmail.com>
2024-05-15 06:32:51 +0100

    ok

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div className='bg-black w-full text-xs  text-green-500 grid place-content-center md:py-8 py-6'>
-            <div>
-                <p className='font-bold'>Copyright &copy; Teamjojocode | 2023.</p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="bg-black w-full text-xs  text-green-500 grid place-content-center md:py-8 py-6">
+      <div>
+        <p className="font-bold">Copyright &copy; Teamjojocode | 2024.</p>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
-
+export default Footer;
